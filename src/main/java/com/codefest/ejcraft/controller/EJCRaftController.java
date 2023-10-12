819a9d1f6299dc93f23b203453a6637bae794c41
chuls <chulsey87@hotmail.com>
2023-10-11 19:37:22 -0500

    Adding todo

--- src/main/java/com/codefest/ejcraft/controller/EJCRaftController.java
+++ src/main/java/com/codefest/ejcraft/controller/EJCRaftController.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EJCRaftController implements ChangeRequestApi {
   @Override
   public ResponseEntity<ChangeRequest> startChangeRequest(List<String> requestBody) {
+    //TODO: Get Info from Jira
+
+    //TODO: Call ChatGPT
+
     ChangeRequest changeRequest = new ChangeRequest();
+
+    //TODO: Replace with data from ChatGPT
     changeRequest.setSummary("Stuff");
     changeRequest.setDescription("More stuff");
 
